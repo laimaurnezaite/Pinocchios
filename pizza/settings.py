15d1ee159ff77be5b54361fcde64083b2eb0b54d
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-04-23 16:28:51 +0300

    create new app pages

--- pizza/settings.py
+++ pizza/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'orders.apps.OrdersConfig',
     'users.apps.UsersConfig',
+    'menu.apps.MenuConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
