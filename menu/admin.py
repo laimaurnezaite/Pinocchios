ff2d22423c8ef01681b12b1b3bdd41e10331711f
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-04-23 16:42:58 +0300

    create and register new menu models

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import MainDish, FreeToppings, Toppings
+
 # Register your models here.
+
+admin.site.register(MainDish)
+admin.site.register(FreeToppings)
+admin.site.register(Toppings)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class MainDish(models.Model):
+    title = models.CharField(max_length = 255)
+    size = models.CharField(max_length = 5, blank=True)
+    unit_price = models.FloatField()
+    item_category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class FreeToppings(models.Model):
+    title = models.CharField(max_length = 255)
+    item_category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Toppings(models.Model):
+    title = models.CharField(max_length = 255)
+    unit_price = models.FloatField()
+    item_category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
