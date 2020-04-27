c74777c33f287801d3711390efe7d51b8e1c9054
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-04-27 10:43:57 +0300

    create food item detail view

--- menu/urls.py
+++ menu/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import MenuListView
+from .views import MenuListView, MenuItemDetailView
 
 urlpatterns = [
     path('', MenuListView.as_view(), name='main_list'),
-    
-
+    path('menu/<int:pk>/', MenuItemDetailView.as_view(), name='item_detail'),    
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
@@ -8,3 +8,7 @@ from .models import Product
 class MenuListView(ListView):
     model = Product
     template_name = 'main_list.html'
+
+class MenuItemDetailView(DetailView):
+    model = Product
+    template_name = 'menu_item_detail.html'
