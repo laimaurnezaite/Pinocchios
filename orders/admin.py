f3692d31fbd3df583eace825f03b5d4aa241394a
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-04-29 18:03:08 +0300

    register models

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Order
+from .models import Order#, #ShoppingCart#, OrderItems
 # Register your models here.
 
 admin.site.register(Order)
+# admin.site.register(ShoppingCart)
+# admin.site.register(OrderItems)
