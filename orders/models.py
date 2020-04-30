fac083da1d234bdb4605402beb1f5941f0117f94
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-04-30 14:40:44 +0300

    make changes in model (sum rename to total)

--- orders/models.py
+++ orders/models.py
@@ -7,24 +7,11 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 # Create your models here.
 
-
-# class ShoppingCart(models.Model):
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE, default = 0)
-#     quantity = models.IntegerField(default = 0)
-#     open = models.BooleanField(default=True)
-
-#     def __str__(self):
-#         return f" Shopping cart {self.id}"
-
-
 class Order (models.Model):
-    # shopping_cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, default = 0)
     date = models.DateTimeField(auto_now_add=True)
     customer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     items = jsonfield.JSONField()
-    # product = models.ForeignKey(Product, on_delete=models.CASCADE, default = 0)
-    # quantity = models.IntegerField(default = 0)
-    sum = models.FloatField(default = 0)
+    total = models.FloatField(default = 0)
     confirmed = models.BooleanField(default=False)
 
     def __str__(self):
