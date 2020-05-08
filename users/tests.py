c555e0bc2e046555799a1d1934a075102a684576
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-08 16:40:27 +0300

    create tests fro Users models

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import CustomUser
 
 # Create your tests here.
+
+class CustomUserModelTest(TestCase):
+
+    def setUp(self):
+        CustomUser.objects.create(
+            username = 'testuser',
+            email = 'testuser@test.com',
+            first_name = 'Test',
+            last_name = 'User',
+            city = 'TestTown'
+        )
+    
+    def test_user_object(self):
+        user = CustomUser.objects.get(id=1)        
+        self.assertEqual(f'{user.username}', 'testuser')
+        self.assertEqual(f'{user.email}', 'testuser@test.com')
+        self.assertEqual(f'{user.first_name}', 'Test')
+        self.assertEqual(f'{user.last_name}', 'User')
+        self.assertEqual(f'{user.city}', 'TestTown')
+
+    
