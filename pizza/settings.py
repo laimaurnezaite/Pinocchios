0a89918ce71d11d0cd3d9b6faae7861ee99b1ea3
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-05-08 14:13:50 +0300

    add sendgrid configuration

--- pizza/settings.py
+++ pizza/settings.py
@@ -133,3 +133,14 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'main_list' 
 LOGOUT_REDIRECT_URL = 'main_list'
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL='laima.urnezaite@gmail.com'
